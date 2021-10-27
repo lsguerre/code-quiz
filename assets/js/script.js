5088aa72578bdbfe790c87c672e315797e9bbf4b
Opal <guerre5lily@gmail.com>
2021-10-26 23:03:27 -0700

    add highscore html, align items on index, add javascript questions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        question: "What does CSS stand for?",
+        choices: ["Central Style Spot", "Cool Stylish Sheets", "Cascading Style Sheets", "Cascada's Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
+
+    {
+        question: "String values are enclosed in _____ when they are assigned in variables",
+        choices: ["periods", "question marks", "parenthesis", "quotation marks"],
+        answer: "quotation marks"
+    },
+
+    {
+        question: "A Boolean value represents which type of value?",
+        choices: ["true/false", "numbers", "strings", "statements"],
+        answer: "true/false"
+
+    },
+
+    {
+        question: "Generally, what is JavaScript used for?",
+        choices: ["Creating coffee related websites", "Making webpages interactive for users", "Reading webpages aloud", "Creating the basic structure of a webpage"],
+        answer: "Making webpages interactive for users"
+    },
+
+    {
+        question: "How would you use an ID selector in CSS and JavaScript?",
+        choices: ["# -hashtag", "!- exlamation point", "?- question mark", ". period"],
+        answer: "# -hashtag"
+    }
+]
